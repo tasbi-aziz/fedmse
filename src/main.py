d926f630fe13d7d9616f96328f7dcda2fd4c9305
Tasbiha Aziz <aziztalukder1212@gmail.com>
2026-09-18 03:04:37 +0600

    Update import statement for manipulate_update

--- src/main.py
+++ src/main.py
@@ -63,7 +63,7 @@ import pandas as pd
 import torch
 import torch.nn as nn
 
-from Trainer import manipulate_update
+from Trainer.malicious_update_experiment2 import manipulate_update
 
 from sklearn.metrics import (
     precision_score,
